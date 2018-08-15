ad7e8870c4cb0de0fa062b10374cf260c2545f8c
venkatsaid <venkatsai9298@gmail.com>
2018-08-15 16:28:46 +0530

    pojo added

---
+++ app/src/main/java/com/example/user/miniproject/Pojo.java
@@ -0,0 +1,26 @@
+package com.example.user.miniproject;
+
+public class Pojo {
+    Boolean s1,s2;
+
+    public Pojo(Boolean s1, Boolean s2) {
+        this.s1 = s1;
+        this.s2 = s2;
+    }
+
+    public Boolean getS1() {
+        return s1;
+    }
+
+    public void setS1(Boolean s1) {
+        this.s1 = s1;
+    }
+
+    public Boolean getS2() {
+        return s2;
+    }
+
+    public void setS2(Boolean s2) {
+        this.s2 = s2;
+    }
+}
