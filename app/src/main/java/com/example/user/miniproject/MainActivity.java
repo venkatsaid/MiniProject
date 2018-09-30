5270219d2196bbd936353299080c60bbaafea9d8
venkatsaid <venkatsai9298@gmail.com>
2018-09-30 18:23:42 +0530

    redundant code reduced

--- app/src/main/java/com/example/user/miniproject/MainActivity.java
+++ app/src/main/java/com/example/user/miniproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.user.miniproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -20,53 +21,112 @@ import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity{
     TextView textView1,textView2;
-    @BindView(R.id.button1)
+//    @BindView(R.id.button1)
     Button button1;
-    @BindView(R.id.button2)
+ //   @BindView(R.id.button2)
     Button button2;
     String string;
     FirebaseDatabase firebaseDatabase;
     DatabaseReference myref;
+    ValueEventListener getDataListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView1=findViewById(R.id.text1);
         textView2=findViewById(R.id.text2);
+        button1=findViewById(R.id.button1);
+        button2=findViewById(R.id.button2);
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference().child("Home");
+        //DatabaseReference myRef = database.getReference().child("Home");
         firebaseDatabase=FirebaseDatabase.getInstance();
-        // myref=firebaseDatabase.getReference("clg1");
-        myref = firebaseDatabase.getReference();
-        myref.addValueEventListener(new ValueEventListener() {
+        getFireData();
+
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-             /*Log.i("count", Long.toString(dataSnapshot.getChildrenCount()));
-                Toast.makeText(MainActivity.this,Long.toString(dataSnapshot.getChildrenCount()), Toast.LENGTH_SHORT).show();
-               Pojo user = (Pojo) dataSnapshot.getValue();
-                Log.i("hiiii", Boolean.toString(user.getId1()));*/
-                /* Toast.makeText(MainActivity.this, Boolean.toString(user.id1), Toast.LENGTH_SHORT).show();*/
-                // Log.d(TAG, "User name: " + user.getName() + ", email " + user.getEmail());
-                collectcolleges((Map<String,Object>) dataSnapshot.getValue());
-            }
+            public void onClick(View v) {
+                myref.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        Pojo pojo=dataSnapshot.getValue(Pojo.class);
+                        if(textView1.getText()=="true"){
+                            textView1.setText("false");
+                            button1.setText("off");
+                            pojo.setS1(false);
+                        }
+                        else {
+                            textView1.setText("true");
+                            button1.setText("On");
+                            pojo.setS1(true);
+                        }
+                        myref.setValue(pojo);
+                    }
 
-            private void collectcolleges(Map<String, Object> value) {
-                ArrayList<Pojo> c=new ArrayList<>();
-                for (Map.Entry<String, Object> entry : value.entrySet()){
-                    Map singleUser = (Map) entry.getValue();
-                    Pojo p=new Pojo((Boolean) singleUser.get("s1"),(Boolean) singleUser.get("s2"));
-                    Toast.makeText(MainActivity.this, Boolean.toString(p.getS1()), Toast.LENGTH_SHORT).show();
-                    c.add(p);
-                }
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
 
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                // Log.w(TAG, "Failed to read value.", error.toException());
+                    }
+                });
             }
         });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myref.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        Pojo pojo=dataSnapshot.getValue(Pojo.class);
+                        if(textView2.getText()=="true"){
+                            textView2.setText("false");
+                            button2.setText("off");
+                            pojo.setS2(false);
+                        }
+                        else {
+                            textView2.setText("true");
+                            button2.setText("On");
+                            pojo.setS2(true);
+                        }
+                        myref.setValue(pojo);
+                    }
 
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+    }
+    private void getFireData(){
+        myref=FirebaseDatabase.getInstance().getReference().child("Home");
+        getDataListener=new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Pojo pojo=dataSnapshot.getValue(Pojo.class);
+                Log.i("pojo",pojo.getS1().toString());
+                if (pojo.getS1()){
+                    textView1.setText("true");
+                    button1.setText("on");
+                }
+                else {
+                    textView1.setText("false");
+                    button1.setText("off");
+                }
+                if (pojo.getS2()){
+                    textView2.setText("true");
+                    button2.setText("on");
+                }
+                else {
+                    textView2.setText("false");
+                    button2.setText("off");
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        myref.addListenerForSingleValueEvent(getDataListener);
     }
 }
--- app/src/main/java/com/example/user/miniproject/Pojo.java
+++ app/src/main/java/com/example/user/miniproject/Pojo.java
@@ -3,10 +3,12 @@ package com.example.user.miniproject;
 public class Pojo {
     Boolean s1,s2;
 
+/*
     public Pojo(Boolean s1, Boolean s2) {
         this.s1 = s1;
         this.s2 = s2;
     }
+*/
 
     public Boolean getS1() {
         return s1;
